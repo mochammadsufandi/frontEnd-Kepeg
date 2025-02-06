d9c7cb449206cc2cf056168d4c0951914ffd412d
mochammadsufandi <mochammaddwi09@gmail.com>
2025-02-07 05:51:19 +0700

    fix : port for running, add choice in drpdown for pendidikanTerakhir, fix typeEffect

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,19 @@ export default function Home() {
     <div>
       <Navbar />
       <div className="pt-[7rem] pb-[2rem] flex justify-evenly">
-        <Image src={"/images/Kejaksaan_Agung_Republik_Indonesia_new_logo.png"} alt="logo-kejaksaan" width={240} height={240}></Image>
+        <Image
+          src={"/images/Kejaksaan_Agung_Republik_Indonesia_new_logo.png"}
+          alt="logo-kejaksaan"
+          width={240}
+          height={240}
+        ></Image>
         <div className="font-bold text-center w-[50%]">
           <TypeEffect
-            strArray={["LAPTRI", 1000, nowDay, 1000]}
-            className="text-6xl text-green-800"
+            strArray={["LAPTRI DAFTAR NAMA PEGAWAI", 1000, nowDay, 1000]}
+            className="text-4xl font-extrabold text-green-800"
           />
-          <div className="mt-[2rem]">
-            <h1 className="text-3xl font-extrabold text-foreground">BIRO KEPEGAWAIAN</h1>
+          <div className="mt-[3rem]">
+            <h1 className="text-3xl font-extrabold text-foreground">SUBBAGIAN KEPEGAWAIAN</h1>
             <h1 className="text-3xl font-extrabold text-foreground">KEJAKSAAN TINGGI JAMBI</h1>
           </div>
         </div>
